69b2fd7b861c22f531b208193ef5b8b763b60349
illia <getapple@kakStifler.local>
2023-02-18 09:11:31 +0200

    banner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { FC } from "react";
+import { Banner } from "./components/banner/banner.component";
 import { Header } from "./components/header/header.components";
 
 interface nameProps { }
 
 export const App: FC<nameProps> = ({ }) => {
-  return <div><Header /></div>
+  return <div>
+    <Header />
+    <Banner />
+  </div>
 }
--- src/components/header/header.components.tsx
+++ src/components/header/header.components.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import { FC } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { Container } from "../container/container.component";
 
 interface HeaderProps { }
 
@@ -12,20 +13,22 @@ export const Header: FC<HeaderProps> = ({ }) => {
         })
     return <header>
         <nav className="px-2 py-4">
-            <div className="mx-auto  max-w-screen-xl flex justify-between items-center">
-                <Link className="font-montseratt text-2xl ml-40 text-mainColor-blue" to="/">Best Blog</Link>
-                <ul className="mr-40 flex list-none">
-                    <li>
-                        <NavLink className={navLinkClasses} to="/home">Home</NavLink>
-                    </li>
-                    <li className="ml-4">
-                        <NavLink className={navLinkClasses} to="/in">Sign in</NavLink>
-                    </li>
-                    <li className="ml-4">
-                        <NavLink className={navLinkClasses} to="/up">Sign up</NavLink>
-                    </li>
-                </ul>
-            </div>
+            <Container>
+                <div className="mx-auto  max-w-screen-xl flex justify-between items-center">
+                    <Link className="font-montseratt text-2xl ml-40 text-mainColor-blue" to="/">Best Blog</Link>
+                    <ul className="mr-40 flex list-none">
+                        <li>
+                            <NavLink className={navLinkClasses} to="/home">Home</NavLink>
+                        </li>
+                        <li className="ml-4">
+                            <NavLink className={navLinkClasses} to="/in">Sign in</NavLink>
+                        </li>
+                        <li className="ml-4">
+                            <NavLink className={navLinkClasses} to="/up">Sign up</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </Container>
         </nav>
     </header>
 }
